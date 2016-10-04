b416839a34565455a8c9492a9cc28091e57beb2c
Evan Schmidt <ejs552@nyu.edu>
2016-10-03 20:48:48 -0400

    added everything so far

---
+++ Assets/scripts/charControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class charControl : MonoBehaviour {
+
+	CharacterController cController;
+
+
+	float jumpTimer;
+	public float xPos;
+	public Transform playerPos;
+	public Transform swarmPos;
+	public Text deathMessage;
+	// Use this for initialization
+	void Start () {
+		cController = GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+
+		cController.SimpleMove (transform.forward * inputY * 8f + transform.right * inputX * 8f);
+
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			jumpTimer = Time.time + 1.2f;
+		}
+		if (Time.time < jumpTimer) {
+			cController.Move (Vector3.up * 0.1f);
+		}
+		xPos = transform.position.x;
+
+		if ((playerPos.position - swarmPos.position).magnitude < 8f) {
+			deathMessage.text = "You've been stung";
+		}
+	}
+}
---
+++ Assets/scripts/swarmControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swarmControl : MonoBehaviour {
+
+	public float swarmX;
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position += new Vector3 (0, 0, -5) * Time.deltaTime;
+		//swarmX = gameObject.GetComponent<charControl> ().xPos;
+	
+
+
+}
+}
